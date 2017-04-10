1595a179fbd53b639ac3687cf75bb82d4fe91e5f
lampo.wei <lampo.wei@fuwo.com>
2017-04-10 15:43:18 +0800

    Add file format to InsertContentToFile.cpp

--- c-cpp/filesystem/InsertContentToFile.cpp
+++ c-cpp/filesystem/InsertContentToFile.cpp
@@ -1,19 +1,38 @@
 /**
  * 用于获取第i行的头的字符在整体文件中的位置(position)
+ * 注意: 要弄清楚一个文件的行尾结束符是'\r\n', 还是'\n'
  *
  * @param buf
  * @param lineNo 指定行号
+ * @param format 文件格式(dos => '\r\n', unix => '\n', mac => '\r')
  *
  * @return size_t
  */
-size_t get_indexed_line_head_position(const std::string& buf, int lineNo)
+size_t get_indexed_line_head_position(const std::string& buf, int lineNo, const std::string& format)
 {
 	size_t location = 0;
+	std::string deli;
+	if (format == "dos")
+	{
+		deli = "\r\n";
+	}
+	else if (format == "unix")
+	{
+		deli = "\n";
+	}
+	else if (format == "mac")
+	{
+		deli = "\r";
+	}
 
 	for (auto i = 0; i < lineNo - 1; i++)
 	{
-		auto index = buf.find("\r\n", location);
-		location = index + 2;
+		auto index = buf.find(deli, location);
+		if (index == -1)
+		{
+			return -1; //表示没有指定的分隔符
+		}
+		location = index + deli.length();
 	}
 
 	return location;
@@ -32,7 +51,7 @@ size_t get_indexed_line_head_position(const std::string& buf, int lineNo)
 void insert_content_to_file(const std::string& filePath, int line_no, const std::string& content)
 {
 	std::string old_content = get_file_content(filePath);
-	size_t third_line_location = get_line_head_index(old_content, line_no);
+	size_t third_line_location = get_line_head_index(old_content, line_no, "dos");
 	std::string head = old_content.substr(0, third_line_location);
 	std::string tail = old_content.substr(third_line_location);
 	std::ofstream fwstream(filePath, std::ios::out | std::ios::trunc);
