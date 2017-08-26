08fe21742f8c5ef8a4fd7a4a12400d4010ea3892
lampo.wei <lampo.wei@fuwo.com>
2017-08-26 13:59:13 +0800

    add new file about base64

---
+++ c-cpp/crypto/base64/Base64.h
@@ -0,0 +1,11 @@
+#ifndef __BASE64_H__
+#define __BASE64_H__
+
+#include <iostream>
+#include <string>
+
+std::string base64_decode(const std::string& ascdata);
+std::string base64_encode(const string& bindata);
+
+#endif
+
